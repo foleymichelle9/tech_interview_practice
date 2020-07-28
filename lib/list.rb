eca8d8800e877d0d784497dcb09ceaf1a6104919
michelle <foleymichelle9@gmail.com>
2020-07-28 11:25:03 -0500

    End timebox

---
+++ lib/list.rb
@@ -0,0 +1,36 @@
+require_relative './node.rb'
+
+class LinkedList
+
+  attr_reader :head
+  
+  def initialize
+    @head = nil
+  end
+
+  def append(surname)
+    if @head == nil
+       @head = Node.new(surname)
+    else
+      node = @head
+    while node.next_node != nil
+      node = node.next_node
+    end
+      node.next_node = Node.new(surname)
+    end
+  end
+
+  def links
+    links = []
+    node = @head
+    while node.next != nil
+      links << node
+      node = node.next_node
+    end
+    links << node
+  end
+
+  def count
+    links.length
+  end
+end
---
+++ lib/node.rb
@@ -0,0 +1,12 @@
+class Node
+
+  attr_reader :surname,
+              :next_node
+
+  def initialize(surname)
+    @surname = surname
+    @next_node = nil
+  end
+end
+
+#head = Node.new(1)
